577407a3a93ef59196a5425d418778bf3e2bd1d0
bryonm <bryon.mondok@gmail.com>
2022-01-24 21:02:44 -0500

    added Section 49 exercises, Restructuring & Flash

--- routes/reviews.js
+++ routes/reviews.js
@@ -19,21 +19,29 @@ const validateReview = (req, res, next) => {
     }
   }
 
-router.post('/', validateReview, catchAsync(async(req, res) => {
-    const campground =  await Campground.findById(req.params.id);
+router.post(
+  '/',
+  validateReview,
+  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
     campground.reviews.push(review);
     await review.save();
     await campground.save();
+    req.flash('success', 'Thank you for leaving your review.');
     res.redirect(`/campgrounds/${campground._id}`);
-  }))
-  
+  })
+);
 
-router.delete('/:reviewId', catchAsync(async (req, res) => {
+router.delete(
+  '/:reviewId',
+  catchAsync(async (req, res) => {
     const { id, reviewId } = req.params;
-    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } })
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
     await Review.findByIdAndDelete(reviewId);
+    req.flash('success', 'Your review has been deleted.');
     res.redirect(`/campgrounds/${id}`);
-  }))
+  })
+);
 
   module.exports = router;
